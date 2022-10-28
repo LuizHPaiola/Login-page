2b813aa46b029d510d22bde50e20d5d1632de9b3
LuizHPaiola <102267230+LuizHPaiola@users.noreply.github.com>
2022-10-28 20:33:11 -0300

    adding the links to the menu component

--- FRONT-END/PROJECT/login/src/Components/Menu.jsx
+++ FRONT-END/PROJECT/login/src/Components/Menu.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Menu(){
     return(
-        <>
-        </>
+    
+        <header>
+            <Link to="/home">Home</Link> | <Link to="/">Login</Link>
+        </header>
+        
     )
 }
--- FRONT-END/PROJECT/login/src/routes/MainRoutes.jsx
+++ FRONT-END/PROJECT/login/src/routes/MainRoutes.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom"
+import Login from '../Components/Login'
+import Home from "../Components/Home"
 
 export default function MainRoutes(){
     return(
         <Routes>
+            <Route path="/" element={<Login/>}/>
+            <Route path="/home" element={<Home/>}/>
         </Routes>
     )
 }
