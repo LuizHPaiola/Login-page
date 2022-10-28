d0ad4c60321bf236032cd491aad16a71fea5b63d
LuizHPaiola <102267230+LuizHPaiola@users.noreply.github.com>
2022-10-28 20:25:05 -0300

    adding routes and the components body

--- FRONT-END/PROJECT/login/src/App.jsx
+++ FRONT-END/PROJECT/login/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Menu from './Components/Menu';
+import MainRoutes from "./routes/MainRoutes"
 
 export default function App(){
 return(
     <div>
+        <Menu/>
         <h1>Hello world</h1>
+        <MainRoutes/>
     </div>
 )
 }
