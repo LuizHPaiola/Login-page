ec04df4e06738b4ac2ead6c744673939fabab80b
LuizHPaiola <102267230+LuizHPaiola@users.noreply.github.com>
2022-10-29 13:06:45 -0300

    adding content to the userBO

--- BACK-END/LoginPage/src/main/java/LoginPage/bo/UserBO.java
+++ BACK-END/LoginPage/src/main/java/LoginPage/bo/UserBO.java
@@ -1,5 +1,13 @@
 package LoginPage.bo;
 
-public class UserBO {
+import LoginPage.dao.UserDAO;
+import LoginPage.to.UserTO;
 
+public class UserBO {
+	UserDAO ud = null;
+	
+	public UserTO validation(UserTO u) {
+		ud = new UserDAO();
+		return ud.loginDAO(u);
+	}
 }
