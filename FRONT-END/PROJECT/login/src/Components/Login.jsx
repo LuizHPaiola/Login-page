5e39a5d29f3e22a2f013642e36c16422040ec0e8
LuizHPaiola <102267230+LuizHPaiola@users.noreply.github.com>
2022-10-29 13:25:56 -0300

    adding content to the login component on react

--- FRONT-END/PROJECT/login/src/Components/Login.jsx
+++ FRONT-END/PROJECT/login/src/Components/Login.jsx
@@ -1,8 +1,74 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function Login(){
+
+    const [user, setUser] = useState({
+        login: "",
+        password: "",
+    });
+
+    const handleChange = (e) =>{
+        setUser({...user, [e.target.name]: e.target.value})
+    }
+
+    const login = async (e) =>{
+        e.preventDefault();
+
+        const requestOptions={
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(user),
+        };
+
+        const response = await fetch(
+            "http://localhost:8080/LoginPage/rest/login",
+            requestOptions
+        );
+
+        const data= await response.json();
+
+        if(data.login){
+            sessionStorage.setItem("valid-user", data.login);
+            window.location = "/home";
+        }else{
+            window.location = "/";
+        }
+    };
+
     return(
-        <>
-        </>
+        <div>
+            <h2>USER LOGIN</h2>
+            <div>
+                <form onSubmit={login}>
+                    <fieldset>
+                        <legend>LOGIN</legend>
+                        <div>
+                            <label htmlFor="idLogin">User</label>
+                            <input 
+                                type="text"
+                                name="login"
+                                id="idLogin"
+                                placeholder="Login"
+                                value={user.login} 
+                                onChange={handleChange}
+                                />
+                        </div>
+                        <div>
+                            <label htmlFor="idPassword">Password</label>
+                                <input 
+                                    type = "password"
+                                    name = "password"
+                                    id = "idPassword"
+                                    placeholder="Password"
+                                    onChange={handleChange}
+                                    />
+                            
+                        </div>
+                        <button>Login</button>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
     )
 }
