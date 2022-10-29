e5ebdb6c2a9be24ea5fed621bdb2b2be031b9c50
LuizHPaiola <102267230+LuizHPaiola@users.noreply.github.com>
2022-10-29 13:09:56 -0300

    adding content to the resource, and finishing the back-end project

--- BACK-END/LoginPage/src/main/java/LoginPage/resource/UserResource.java
+++ BACK-END/LoginPage/src/main/java/LoginPage/resource/UserResource.java
@@ -1,7 +1,28 @@
 package LoginPage.resource;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import LoginPage.bo.UserBO;
+import LoginPage.to.UserTO;
 
 @Path("/login")
 public class UserResource {
+	
+	UserBO userBO = new UserBO();
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	
+	public Response userLogin(UserTO user) {
+			return Response
+					.status(200)
+					.entity(userBO.validation(user))
+					.build();
+	}
 }
